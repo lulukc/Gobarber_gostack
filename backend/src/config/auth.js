6831a00630fa350d97806d3e74401d09df09fccc
Lucas <lhamancio@gmail.com>
2019-10-20 18:55:23 -0300

    validação dos dados de entrada

---
+++ backend/src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '662160c750dbb35ba3c07e33ad8d509c',
+  exprireIn: '7d',
+};
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,8 +1,16 @@
 import { Router } from 'express';
 import UserController from './app/controllers/userController';
+import SessionController from './app/controllers/sessionController';
+import authController from './app/middlewares/auth';
 
 const routes = new Router();
 
 routes.post('/users', UserController.store);
+
+routes.post('/session', SessionController.store);
+
+routes.use(authController);
+
+routes.put('/users', UserController.update);
 
 export default routes;
